0ddeec4430c1c7157b6b77863f61bd7643e13d01
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2024-07-17 08:20:49 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -12,6 +12,39 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
+class Customer(db.Model):
+    __tablename__ = 'customers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Customer {self.id}, {self.name}>'
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+
+
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
